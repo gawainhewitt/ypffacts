cf76a619e7def020b3371d6a0377325c7f9a4899
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2025-01-28 15:21:52 +0000

    ammendments made to text as per Victoria's email

--- ypfFacts.h
+++ ypfFacts.h
@@ -4,8 +4,8 @@ const int MAX_SIZE = 230;
 char ypfFacts [NUMBER_OF_ELEMENTS] [MAX_SIZE] = {
 {"The Young People's Forum started way back in 2012 - there were only 10 members then"}, 
 {"There are currently over 60 members of the Young People's Forum and there have been over 200 members since it started in 2012"}, 
-{"In 2023 the Young People's Forum were involved in over 90 projects to help improve patient experience"}, 
-{"In 2017 GOSH Young People's Forum hosted the very first Big Youth meet Up"}, 
-{"There are 6 YPF meetings a year and members receive a hoodie at their sixth meeting"}, 
+{"In 2023 members of the Young People's Forum were involved in over 90 projects to help improve patient experience"}, 
+{"In 2017 GOSH Young People's Forum hosted the very first Big Youth Meet-Up"}, 
+{"There are 6 YPF meetings a year and members receive a hoodie at their 6th meeting"}, 
 {"Members of the Young People's Forum are aged between 10 - 21 and are GOSH patients and their siblings"}, 
 };
